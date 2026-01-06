dd2f2591f84aacc07216bb462c8b89df6feb74de
patluka <lukaszewski.patryk06@gmail.com>
2026-01-06 13:45:03 +0100

    Add action to start time in trial to GUI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QBrush
 from connection_manager import ConnectionManager
 from gui.section_lane_control_panel import SectionLaneControlPanel
 from gui.section_clearoff_fast import SectionClearOffTest
-from gui.setting_option import SettingTurnOnPrinter
+from gui.setting_option import SettingTurnOnPrinter, SettingStartTimeInTrial
 from gui.socket_section import SocketSection
 from log_management import LogManagement
 from config_reader import ConfigReader, ConfigReaderError
@@ -94,6 +94,7 @@ class GUI(QDialog):
         self.__section_clearoff_fast = SectionClearOffTest()
 
         self.__action_setting_turn_on_printer = SettingTurnOnPrinter(self)
+        self.__action_setting_start_time_in_trial = SettingStartTimeInTrial(self)
 
         self.__set_layout()
         self.__init_program()
@@ -197,6 +198,7 @@ class GUI(QDialog):
 
             self.__connection_manager.add_func_for_analyze_msg_to_lane(lambda msg: self.__section_clearoff_fast.analyze_message_to_lane(msg))
             self.__connection_manager.add_func_for_analyze_msg_to_lane(lambda msg: self.__action_setting_turn_on_printer.analyze_message_to_lane(msg))
+            self.__connection_manager.add_func_for_analyze_msg_to_lane(lambda msg: self.__action_setting_start_time_in_trial.analyze_message_to_lane(msg))
 
             start_new_thread(self.__connection_manager.start, ())
         except ConfigReaderError as e:
@@ -276,6 +278,7 @@ class GUI(QDialog):
 
         settings = menu_bar.addMenu("Ustawienia")
         settings.addAction(self.__action_setting_turn_on_printer.get_menu_action())
+        settings.addAction(self.__action_setting_start_time_in_trial.get_menu_action())
 
         ip_menu = menu_bar.addMenu("Adresy IP")
         ip_refresh_action = QAction("Odśwież listę adresów IP", self)
